d1118a1cbbc9bf7cf55bcf2ed08c6c567c125be7
s-anti <89141124+s-anti@users.noreply.github.com>
2023-10-22 23:47:16 -0300

    cambiador de temas

--- src/elements/header.svelte
+++ src/elements/header.svelte
@@ -1,46 +1,47 @@
 <script>
+	var oscuro = false;
 	function reloadTheme() {
 		if (
 			localStorage.theme === 'dark' ||
 			(!('theme' in localStorage) && window.matchMedia('(prefers-color-scheme: dark)').matches)
 		) {
 			document.documentElement.classList.add('dark');
+			oscuro = true;
 		} else {
 			document.documentElement.classList.remove('dark');
+			oscuro = false;
 		}
 	}
 </script>
 
-<header class="flex justify-between p-2 items-center fixed z-40 top-0 w-full">
-	<div class="flex-1 justify-center flex">asd</div>
-	<div class="flex-1 justify-center flex">asd</div>
+<header class="flex justify-between p-2 items-center fixed z-40 top-0 w-full backdrop:blur-3xl">
 	<div class="flex-1 justify-center flex gap-2">
-		<button
-			on:click={() => {
-				localStorage.theme = 'light';
+		{#if oscuro}
+			<button
+				class="material-icons"
+				on:click={() => {
+					localStorage.theme = 'light';
 
-				reloadTheme();
-			}}
-		>
-			Blacn
-		</button>
-
+					reloadTheme();
+				}}>dark_mode</button
+			>
+		{:else}
+			<button
+				class="material-icons"
+				on:click={() => {
+					localStorage.theme = 'dark';
+					reloadTheme();
+				}}
+			>
+				light_mode
+			</button>
+		{/if}
+	</div>
+	<div class="flex-1 justify-center flex">2023</div>
+	<div class="flex-1 justify-center flex">
 		<button
-			on:click={() => {
-				localStorage.theme = 'dark';
-				reloadTheme();
-			}}
+			class="bg-primary text-text flex items-center justify-center p-2 rounded-full gap-2 hover:rotate-6 transition-all"
+			>Contacto <span class="material-icons">add_ic_call</span></button
 		>
-			nego
-		</button>
-
-		<button
-			on:click={() => {
-				localStorage.removeItem('theme');
-				reloadTheme();
-			}}
-		>
-			defaul
-		</button>
 	</div>
 </header>
